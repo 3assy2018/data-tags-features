87b3c00686aec4d58cd8eaac5cf01334e7b5ce5c
Mohamed <3assy@diva-lab.com>
2020-01-30 15:59:48 +0200

    Validation Edits

--- src/Models/DataTagGroup.php
+++ src/Models/DataTagGroup.php
@@ -3,6 +3,7 @@
 namespace M3assy\DataTags\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class DataTagGroup extends Model
 {
@@ -23,5 +24,13 @@ class DataTagGroup extends Model
     public function tags()
     {
         return $this->hasMany(DataTag::class, "group_id");
+    }
+
+    public function delete()
+    {
+        $path = config('datatags.default_path');
+        $path = $path[strlen($path) - 1] == "/" ? $path . $this->directory : $path . "/" . $this->directory;
+        Storage::disk('local')->deleteDirectory($path);
+        return parent::delete();
     }
 }
--- src/Traits/HasDataTagGenerator.php
+++ src/Traits/HasDataTagGenerator.php
@@ -2,24 +2,33 @@
 
 namespace M3assy\DataTags\Traits;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use M3assy\DataTags\Facades\DataTag;
 use M3assy\DataTags\Models\DataTagGroup;
 
 trait HasDataTagGenerator
 {
-    public function generateDataTagsAndSave($prefix = null, $generationType = "text")
+    public function generateDataTagsAndSave($request = null,$prefix = null, $generationType = "text")
     {
         $tokens = [];
 
-        $request = request();
+        $request = $request ?? request()->all();
         $mappers = config("datatags.mappers");
-        $inputs = $request->only(array_values($mappers));
-        $mappedInputs = array_combine(array_keys($mappers), array_values($inputs));
+        $mappedInputs = [];
+        foreach ($mappers as $key => $value){
+            $mappedInputs[$key] = array_key_exists($value, $request) ? $request[$value] : null;
+        }
+        if(!array_key_exists($mappers["type"], $request)){
+            $mappedInputs[$mappers["type"]] = config('datatags.default_generator');
+        }
+        else{
+            $mappedInputs[$mappers['type']] = $mappedInputs[$mappers['type']] ?? config('datatags.default_generator');
+        }
         $directory = hash_hmac("md5", time(), config("datatags.file_hmac_key"));
         $mappedInputs["directory"] = $directory;
 
-        $type = $request->input($mappers["type"]);
+        $type = $mappedInputs[$mappers["type"]];
         $dataTagClient = DataTag::type($type);
 
         $dataGroup = $this->dataTagsGroups()->create($mappedInputs);
@@ -28,7 +37,7 @@ trait HasDataTagGenerator
             $token = hash_hmac("md5",
                 $mappedInputs["name"].$mappedInputs["number"].$mappedInputs["expiration_date"].microtime(),
                 config("datatags.file_hmac_key"));
-            $output = $dataTagClient->generate($prefix."/".$token, $generationType);
+            $output = $dataTagClient->generate(($prefix ?? config('datatags.default_prefix'))."/".$token, $generationType);
             $dataOutputFormat = config("datatags.generators.".$type.".config.format");
             Storage::disk("local")
                 ->put(config("datatags.default_path")."/".$directory."/".$token.".".$dataOutputFormat, $output);
